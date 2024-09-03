b63f40d29bee4b248acaa264187c511ff331c122
luigiikk <luigimee15@gmail.com>
2024-09-03 00:39:59 -0300

    first commit: config all project

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const port = 3333;
+
+const connectionString = process.env.CONNECTIONSTRING;
+
+if (!connectionString) {
+  throw new Error("A variável de ambiente CONNECTIONSTRING não está definida!");
+}
+
+mongoose
+  .connect(connectionString)
+  .then(() => {
+    const app = express();
+    app.get("/", (req, res) => {
+      res.send("Olá mundo");
+    });
+
+    app.listen(port, () => {
+      console.log(`Estou conectado na porta: ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Erro ao conectar ao MongoDB:", error);
+  });
